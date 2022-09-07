75e65c3cf27f27d44872bb9db1dc0b9bbb809d0e
alper ademoglu <alperademoglu@hotmail.com>
2022-09-07 13:49:20 -0400

    Feature 8 Complete

--- todos/views.py
+++ todos/views.py
@@ -6,4 +6,10 @@ from todos.models import TodoList, TodoItem
 def TodoListListView(request):
     context = {"todo_list_list": TodoList.objects.all()}
 
-    return render(request, "list.html", context)
+    return render(request, "todos/list.html", context)
+
+
+def TodoListDetailView(request, pk):
+    context = {"todo_list_detail": TodoList.objects.get(pk=pk)}
+
+    return render(request, "todos/detail.html", context)
