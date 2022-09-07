2b502dba4ac7455996b1eea47b6af6e8f63f99fa
alper ademoglu <alperademoglu@hotmail.com>
2022-09-07 12:01:09 -0400

    Feature 3 Complete

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
+USER_MODEL = settings.AUTH_USER_MODEL
 # Create your models here.
+
+
+class TodoList(models.Model):
+    name = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
